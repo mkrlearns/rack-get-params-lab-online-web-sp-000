5cce4c1fd8d8e0a294cce85efc5fd6f3a8761496
ryan m <contact@ryanmeek.com>
2020-02-17 01:28:04 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,30 +1,28 @@
 class Application
-
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
+      @@items.each { |i| resp.write "#{i}\n" }
     elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    else
-      resp.write "Path Not Found"
-    end
+      resp.write handle_search(req.params["q"])
+    elsif req.path.match(/cart/)
+      if @@cart.length == 0; resp.write "Your cart is empty"
+      else; @@cart.each { |i| resp.write "#{i}\n" } end
+    elsif req.path.match(/add/)
+      item = req.params["item"]
+      if @@items.include?(item); @@cart << item; resp.write "added #{item}"
+      else; resp.write "We don't have that item" end
+    else; resp.write "Path Not Found" end
 
     resp.finish
   end
 
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
+  def handle_search(s)
+    @@items.include?(s) ? "#{s} is one of our items" : "Couldn't find #{s}"
   end
 end
